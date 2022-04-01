4839dd29e1b97bff2f9ff9f5f384c1d5cb5b621a
Mustafa Budce <mustafabudce@gmail.com>
2022-04-01 20:07:35 +0200

    File was created.

---
+++ Print Odd_Number or Even_Number.py
@@ -0,0 +1,8 @@
+
+num = input("Enter a number:")
+if (int(num) % 2 == 0) :
+
+    print("The number you entered", num, "is an even number." )
+else :
+
+    print("The number you entered", num, "is an odd number.")
