8aa4b43360c9f82322cefc7dfbbc6c58576885a8
Mustafa Budce <mustafabudce@gmail.com>
2022-04-03 14:09:57 +0200

    Same file was deleted

--- Print Which_One_is_Big.py
+++
@@ -1,9 +0,0 @@
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
-
-if first_number > second_number:
-    print('{} is bigger than {}'.format(first_number, second_number))
-else:
-    print('{} is bigger than {}'.format(second_number, first_number))
-
-
