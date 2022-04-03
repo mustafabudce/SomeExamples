e35bc22e05a051a93651ab3527720332ff6e3bb8
Mustafa Budce <mustafabudce@gmail.com>
2022-04-03 13:13:06 +0200

    File was created.

---
+++ Print Area_of_The_Circle.py
@@ -0,0 +1,5 @@
+r = int(input("Please enter an integer radius:"))
+PI_number = 22 / 7
+PI_number = round(PI_number, 3)
+area_of_the_circle = PI_number * r * r
+print('Area of circle : {}'.format(area_of_the_circle))
